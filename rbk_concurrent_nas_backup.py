2d0b2790d54b0b0b702434e377aa6345f8e75fcf
adamfox <adamrfox@yahoo.com>
2021-07-28 17:03:03 -0400

    rbk_concurrent_nas_backup.py: First cut of restartability

--- rbk_concurrent_nas_backup.py
+++ rbk_concurrent_nas_backup.py
@@ -4,6 +4,8 @@ import sys
 import time
 import getopt
 import getpass
+from datetime import datetime
+import operator
 import urllib3
 urllib3.disable_warnings()
 import rubrik_cdm
@@ -48,6 +50,7 @@ def get_fs_id(hs_id, fs_data):
 
 def get_job_queue(hs_data, sla_data, fs_data, infile, def_sla):
     new_job_queue = []
+    print("Generating Job Queue")
     with open(infile) as fp:
         for line in fp:
             if not line or line.startswith('#'):
@@ -74,7 +77,33 @@ def get_job_queue(hs_data, sla_data, fs_data, infile, def_sla):
                 continue
             new_job_queue.append({'host': lf[0], 'share': lf[1], 'hs_id': hs_id, 'sla_id': sla_id, 'fs_id': fs_id_list[0]})
     fp.close()
-    return(new_job_queue)
+    if RESTART:
+        epoch = datetime.strptime("1970-01-01T00:00:00", "%Y-%m-%dT%H:%M:%S")
+        job_delete_queue = []
+        running_jobs = []
+        for j in new_job_queue:
+            j_run = rubrik.get('v1', '/event/latest?event_status=Running&event_type=Backup&object_ids=' + str(j['hs_id']) + ',' + str(j['fs_id']))
+            try:
+                rj_id = j_run['data'][0]['latestEvent']['jobInstanceId']
+                url = "/fileset/request/" + str(rj_id)
+                running_jobs.append({'host': j['host'], 'share': j['share'], 'status': url})
+                job_delete_queue.append(j)
+            except:
+                j_inst = rubrik.get('v1', '/event/latest?event_status=Success&event_type=Backup&object_ids=' + str(j['hs_id']) + ',' + str(j['fs_id']))
+                try:
+                    time = j_inst['data'][0]['latestEvent']['time']
+                    time_dt = datetime.strptime(time[:-5], "%Y-%m-%dT%H:%M:%S")
+                    j['time'] = int((time_dt - epoch).total_seconds())
+                except:
+                    j['time'] = 0
+        for dj in job_delete_queue:
+            new_job_queue.remove(dj)
+        new_job_queue.sort(key=operator.itemgetter('time'))
+        dprint("\nJOB_QUEUE: " + str(new_job_queue))
+        print("QUEUE LENGTH:" + str(len(new_job_queue)))
+        print("ALREADY RUNNING JOBS: " + str(len(running_jobs)))
+        return(new_job_queue, running_jobs)
+    return(new_job_queue, [])
 
 if __name__ == "__main__":
     user = ""
@@ -89,8 +118,10 @@ if __name__ == "__main__":
     timeout = 60
     NAS_DA = False
     running_status_list = ['RUNNING', 'QUEUED', 'ACQUIRING', 'FINISHING']
+    RESTART = True
 
-    optlist, args = getopt.getopt(sys.argv[1:], 'hDc:t:ms:', ['--help', '--DEBUG', '--creds=', '--token=', '--max_jobs=', '--sla='])
+    optlist, args = getopt.getopt(sys.argv[1:], 'hDc:t:ms:f', ['--help', '--DEBUG', '--creds=', '--token=', '--max_jobs=',
+                                                               '--sla=', '--flush'])
     for opt, a in optlist:
         if opt in ('-h', '--help'):
             usage()
@@ -104,6 +135,8 @@ if __name__ == "__main__":
             max_jobs = int(a)
         if opt in ('-s', '--sla'):
             default_sla = a
+        if opt in ('-f', '--flush'):
+            RESTART = False
 
     try:
         (infile, rubrik_host) = args
@@ -130,7 +163,7 @@ if __name__ == "__main__":
     if fs_data['total'] == 0:
         sys.stderr.write("No Filesets found\n")
         exit(3)
-    job_queue = get_job_queue(hs_data, sla_data, fs_data, infile, default_sla)
+    (job_queue, jobs_running) = get_job_queue(hs_data, sla_data, fs_data, infile, default_sla)
     while(job_queue or jobs_running):
         if len(jobs_running) < max_jobs:
             try:
@@ -138,11 +171,14 @@ if __name__ == "__main__":
                 print ("Starting Backup of " + new_job['host'] + ":" + new_job['share'])
                 bu_config = {'slaId': new_job['sla_id'], 'isPassthrough': NAS_DA}
                 bu_status = rubrik.post('v1', '/fileset/' + str(new_job['fs_id']) + "/snapshot", bu_config, timeout=timeout)
+                dprint("JOB: " + str(bu_status))
                 bu_status_url = str(bu_status['links'][0]['href']).split('/')
                 bu_status_path = "/" + "/".join(bu_status_url[5:])
                 jobs_running.append({'host': new_job['host'], 'share': new_job['share'], 'status': bu_status_path})
+                continue
             except:
                 pass
+        remove_queue = []
         for j in jobs_running:
             j_status = rubrik.get('v1', j['status'], timeout=timeout)
             job_status = str(j_status['status'])
@@ -150,8 +186,11 @@ if __name__ == "__main__":
             if job_status in running_status_list:
                 continue
             else:
-                jobs_running.remove(j)
-        print('\n')
+                remove_queue.append(j)
+        for rj in remove_queue:
+            jobs_running.remove(rj)
+        print("Queued Jobs: " + str(len(job_queue)))
+        print('')
         time.sleep(10)
 
 
