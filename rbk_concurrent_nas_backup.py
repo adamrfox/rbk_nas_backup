ff157260bfa6ee9233ca58da7ab6dafac8c824b0
adamfox <adamrfox@yahoo.com>
2021-08-03 17:37:40 -0400

    rbk_concurrent_nas_backup.py: More customer requests - Show % of queue done - Better restart by figuring out completed jobs using a log file.  Maintains job order - DA a flag option - Longer wait time between screen udpates while still firing up a new job ever 10 seconds

--- rbk_concurrent_nas_backup.py
+++ rbk_concurrent_nas_backup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from __future__ import print_function
+from __future__ import division
 import sys
 import time
 import getopt
@@ -9,6 +10,9 @@ import operator
 import urllib3
 urllib3.disable_warnings()
 import rubrik_cdm
+import os.path
+from os import path
+
 
 
 def usage():
@@ -48,9 +52,64 @@ def get_fs_id(hs_id, fs_data):
             continue
     return(fs_id_list)
 
-def get_job_queue(hs_data, sla_data, fs_data, infile, default_host, def_sla, default_fileset):
+def rewrite_log_file(log_file, done_jobs):
+    lc = open(log_file, "w")
+    for dj in done_jobs:
+        lc.write(dj['host'] + "," + dj['share'] + ",SUCCEEDED," + dj['date'] + "\n")
+    lc.close()
+    return
+
+def compare_job_queues(done, new):
+    for n in new:
+        found = False
+        for dj in done:
+            if dj['host'] == n['host'] and dj['share'] == n['share']:
+                found = True
+                break
+        print("FOUND: " + str(found))
+        if not found:
+            return(False)
+    print("FUNC RETURNING TRUE")
+    return(True)
+
+def check_job_status_log(log_file, new_job_queue):
+    done_jobs = []
+    to_do = False
+    if not path.isfile(log_file):
+        return([])
+    with open(log_file) as fp:
+        for line in fp:
+            line = line.rstrip()
+            if not line:
+                continue
+            (host, share, status, date)  = line.split(',')
+            if status == "SUCCEEDED":
+                done_jobs.append({'host': host, 'share': share, 'date': date})
+            else:
+                to_do = True
+    fp.close()
+    if compare_job_queues(done_jobs, new_job_queue):
+        return([])
+    if to_do:
+        rewrite_log_file(log_file, done_jobs)
+    return(done_jobs)
+
+def get_fst_id(rubrik, def_fst):
+    fst_data = rubrik.get('v1', '/fileset_template?name=' + def_fst, timeout=timeout)
+    try:
+        return(fst_data['data'][0]['id'])
+    except:
+        return("")
+
+def add_template_to_share(hs_id, fst_id):
+    payload = [{'shareId': hs_id, 'templateId': fst_id, 'isPassthrough': NAS_DA, 'enableSymlinkResolution': False, 'enableHardlinkSupport': False}]
+    print("PAYLOAD " + str(payload))
+    new_fs = rubrik.post('internal', '/fileset/bulk', payload, timeout=timeout)
+    return(new_fs['data'][0]['id'])
+
+def get_job_queue(hs_data, sla_data, fs_data, infile, default_host, def_sla, def_fst_id):
     new_job_queue = []
-    def_fst_id = ""
+    completed_job_queue = []
     print("Generating Job Queue")
     with open(infile) as fp:
         for line in fp:
@@ -66,7 +125,7 @@ def get_job_queue(hs_data, sla_data, fs_data, infile, default_host, def_sla, def
                 share = lf[1]
             hs_id = get_hs_id(hs_data, host, share)
             if hs_id == "":
-                sys.stderr.write("Can't find " + lf[0] + ":" + lf[1] + ". Skipping\n")
+                sys.stderr.write("Can't find " + host + ":" + share + ". Skipping\n")
                 continue
             if def_sla:
                 sla_id = get_sla_id(sla_data, def_sla)
@@ -79,16 +138,7 @@ def get_job_queue(hs_data, sla_data, fs_data, infile, default_host, def_sla, def
                     continue
             fs_id_list = get_fs_id(hs_id, fs_data)
             if len(fs_id_list) == 0:
-                if not default_fileset:
-                    sys.stderr.write("Can't find fileset for " + host + ":" + share + ". Skipping\n")
-                    continue
-                if not def_fst_id:
-                    for f in fs_data['data']:
-                        if f['name'] == default_fileset:
-                            def_fst_id = f['id']
-                            fs_id = f['id']
-                else:
-                    fs_id = f['id']
+                fs_id = add_template_to_share(hs_id, def_fst_id)
             elif len(fs_id_list) > 1:
                 sys.stderr.write("Found multiple filsets for " + host + ":" + share + ". Skipping\n")
                 continue
@@ -108,22 +158,50 @@ def get_job_queue(hs_data, sla_data, fs_data, infile, default_host, def_sla, def
                 running_jobs.append({'host': j['host'], 'share': j['share'], 'status': url})
                 job_delete_queue.append(j)
             except:
-                j_inst = rubrik.get('v1', '/event/latest?event_status=Success&event_type=Backup&object_ids=' + str(j['hs_id']) + ',' + str(j['fs_id']))
-                try:
-                    time = j_inst['data'][0]['latestEvent']['time']
-                    time_dt = datetime.strptime(time[:-5], "%Y-%m-%dT%H:%M:%S")
-                    j['time'] = int((time_dt - epoch).total_seconds())
-                except:
-                    j['time'] = 0
+                if SORT_ON_TIME:
+                    j_inst = rubrik.get('v1', '/event/latest?event_status=Success&event_type=Backup&object_ids=' + str(j['hs_id']) + ',' + str(j['fs_id']))
+                    try:
+                        time = j_inst['data'][0]['latestEvent']['time']
+                        time_dt = datetime.strptime(time[:-5], "%Y-%m-%dT%H:%M:%S")
+                        j['time'] = int((time_dt - epoch).total_seconds())
+                    except:
+                        j['time'] = 0
         for dj in job_delete_queue:
             new_job_queue.remove(dj)
+        completed_job_queue = check_job_status_log(log_file, new_job_queue)
+        print("NEW JOBS: " + str(new_job_queue))
+        print("COMPLETED: " + str(completed_job_queue))
+        if completed_job_queue:
+            print("Purging Completed Jobs")
+            for cj in completed_job_queue:
+                for j in new_job_queue:
+                    if j['host'] == cj['host'] and j['share'] == cj['share']:
+                        new_job_queue.remove(j)
+                        break
+        else:
+            fp = open(log_file, "w")
+            fp.close()
         if SORT_ON_TIME:
             new_job_queue.sort(key=operator.itemgetter('time'))
         dprint("\nJOB_QUEUE: " + str(new_job_queue))
         print("QUEUE LENGTH:" + str(len(new_job_queue)))
         print("ALREADY RUNNING JOBS: " + str(len(running_jobs)))
-        return(new_job_queue, running_jobs)
-    return(new_job_queue, [])
+        return(new_job_queue, running_jobs, completed_job_queue)
+    return(new_job_queue, [], [])
+
+def log_job(lfile, job, job_status):
+    date_s = datetime.now().strftime("%Y-%m-%dT%H:%M")
+    fp = open(lfile, "a")
+    fp.write(job['host'] + "," + job['share'] + "," + job_status + "," + date_s + "\n")
+    fp.close()
+    return
+
+def print_job_report(job, status, j_cnt):
+    date_s = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    if j_cnt == 1:
+        print(date_s)
+    print ("\tJOB STATUS: " + job['host'] + ":" + job['share'] + " : " + status)
+    return
 
 if __name__ == "__main__":
     user = ""
@@ -131,7 +209,8 @@ if __name__ == "__main__":
     token = ""
     DEBUG = False
     job_queue = []
-    job_index = 0
+    job_success = []
+    job_fail = []
     max_jobs = 2
     jobs_running = []
     default_sla = ""
@@ -139,13 +218,16 @@ if __name__ == "__main__":
     default_fileset = ""
     timeout = 60
     NAS_DA = False
-    running_status_list = ['RUNNING', 'QUEUED', 'ACQUIRING', 'FINISHING']
+    running_status_list = ['RUNNING', 'QUEUED', 'ACQUIRING', 'FINISHING', 'TO_CANCEL']
     RESTART = True
     SORT_ON_TIME = False
+    log_file = "job_log.csv"
+    REPORT_DELAY = 0
+    pct_done = 0.0
 
-    optlist, args = getopt.getopt(sys.argv[1:], 'hDc:t:ms:Fn:f:dS', ['--help', '--DEBUG', '--creds=', '--token=', '--max_jobs=',
+    optlist, args = getopt.getopt(sys.argv[1:], 'hDc:t:ms:Fn:f:dSr:', ['--help', '--DEBUG', '--creds=', '--token=', '--max_jobs=',
                                                                '--sla=', '--flush', '--nas_host=', '--fileset=', '--nas_da',
-                                                                '--sort_on_time'])
+                                                                '--sort_on_time', '--report_time='])
     for opt, a in optlist:
         if opt in ('-h', '--help'):
             usage()
@@ -169,12 +251,14 @@ if __name__ == "__main__":
             NAS_DA = True
         if opt in ('-S', '--sort_on_time'):
             SORT_ON_TIME = True
+        if opt in ('-r', '--report_time'):
+            REPORT_DELAY = int(a)
 
     try:
         (infile, rubrik_host) = args
     except:
         usage()
-
+    sleep_cycles = REPORT_DELAY*60/10
     if token:
         rubrik = rubrik_cdm.Connect(rubrik_host, api_token=token)
     else:
@@ -183,6 +267,11 @@ if __name__ == "__main__":
         if not password:
             password = getpass.getpass("Password: ")
         rubrik = rubrik_cdm.Connect(rubrik_host, user, password)
+    if default_fileset:
+        def_fst_id = get_fst_id(rubrik, default_fileset)
+        if not def_fst_id:
+            sys.stderr.write("Can't find default fileset template: " + default_fileset + "\n")
+            exit(2)
     hs_data = rubrik.get('internal', '/host/share', timeout=timeout)
     if hs_data['total'] == 0:
         sys.stderr.write("No NAS Shares found.\n")
@@ -195,10 +284,16 @@ if __name__ == "__main__":
     if fs_data['total'] == 0:
         sys.stderr.write("No Filesets found\n")
         exit(3)
-    (job_queue, jobs_running) = get_job_queue(hs_data, sla_data, fs_data, infile, default_host, default_sla, default_fileset)
+    (job_queue, jobs_running, job_success) = get_job_queue(hs_data, sla_data, fs_data, infile, default_host, default_sla, def_fst_id)
+    original_job_queue = len(job_queue) + len(jobs_running) + len(job_success)
+    report_cycle = 0
     while(job_queue or jobs_running):
-        if len(jobs_running) < max_jobs:
+        report_cycle += 1
+        if report_cycle > sleep_cycles:
+            report_cycle = 1
+        if (len(jobs_running) < max_jobs) and job_queue:
             try:
+                report_cycle = 1
                 new_job = job_queue.pop(0)
                 print ("Starting Backup of " + new_job['host'] + ":" + new_job['share'])
                 bu_config = {'slaId': new_job['sla_id'], 'isPassthrough': NAS_DA}
@@ -207,22 +302,41 @@ if __name__ == "__main__":
                 bu_status_url = str(bu_status['links'][0]['href']).split('/')
                 bu_status_path = "/" + "/".join(bu_status_url[5:])
                 jobs_running.append({'host': new_job['host'], 'share': new_job['share'], 'status': bu_status_path})
-                continue
             except:
                 pass
         remove_queue = []
+        j_cnt = 0
         for j in jobs_running:
+            j_cnt += 1
             j_status = rubrik.get('v1', j['status'], timeout=timeout)
             job_status = str(j_status['status'])
-            print("JOB STATUS: " + j['host'] + ":" + j['share'] + " : " + job_status)
+            if report_cycle == 1:
+                print_job_report(j, job_status, j_cnt)
             if job_status in running_status_list:
                 continue
             else:
+                log_job(log_file, j, job_status)
+                j['status'] = job_status
+                if job_status == "SUCCEEDED":
+                    job_success.append(j)
+                else:
+                    job_fail.append(j)
                 remove_queue.append(j)
         for rj in remove_queue:
             jobs_running.remove(rj)
-        print("Queued Jobs: " + str(len(job_queue)))
-        print('')
+        if report_cycle == 1:
+            print("\tQueued Jobs: " + str(len(job_queue)))
+            job_num = len(job_success) + len(job_fail)
+            pct_done = (job_num / original_job_queue) * 100
+            print("\tQueue Progress: " + str(round(pct_done)) + "%")
+            print('')
         time.sleep(10)
-
-
+    if job_fail:
+        print(str(len(job_fail)) + " Failed Job", end='')
+        if len(job_fail) == 1:
+            print(':')
+        else:
+            print('s:')
+        for fj in job_fail:
+            print("\t" + fj['host'] + ":" + fj['share'] + " : " + fj['status'])
+    print("\nDone!")
